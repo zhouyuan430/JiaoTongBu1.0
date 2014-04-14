506c57bbe0ed44d5646a4b1384ac809060362c81
zhouyuan <zhou_yuan1993@163.com>
2014-04-14 21:24:08 +0800

    下载图片类

---
+++ Commen/AssetImage.h
@@ -0,0 +1,13 @@
+//
+//  AssetImage.h
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-14.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssetImage : UIImageView
+
+@end
---
+++ Commen/AssetImage.m
@@ -0,0 +1,31 @@
+//
+//  AssetImage.m
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-14.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+//
+
+#import "AssetImage.h"
+
+@implementation AssetImage
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
