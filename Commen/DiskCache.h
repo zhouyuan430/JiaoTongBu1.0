25f006159f5cc71018e8e42532da6e620f7acd68
zhouyuan <zhou_yuan1993@163.com>
2014-04-14 20:36:36 +0800

    本地缓存类

---
+++ Commen/DiskCache.h
@@ -0,0 +1,13 @@
+//
+//  DiskCache.h
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-14.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiskCache : NSObject
+
+@end
