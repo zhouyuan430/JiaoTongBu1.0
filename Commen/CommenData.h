f2a4b9492ed0b163afe0cc8c6fb63046061f740f
zhouyuan <zhou_yuan1993@163.com>
2014-04-17 00:54:45 +0800

    临时缓存类  此缓存再应用退出后就会被删除

---
+++ Commen/CommenData.h
@@ -0,0 +1,26 @@
+//
+//  CommenData.h
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-17.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+
+//缓存临时文件  程序退出 数据删除
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommenData : NSObject
+
++ (CommenData *) mainShare;
+
+//获取本地信息
+-(NSDictionary*)getInfo:(NSString *)fileName;
+//记录本地
+-(void)saveInfo:(NSDictionary*)dict fileName:(NSString*)fileName;
+//删除文件
+-(Boolean)DeleteFile:(NSString*)fileName;
+//检查文件是否存在
+-(Boolean)isExistsFile:(NSString*)fileName;
+
+@end
